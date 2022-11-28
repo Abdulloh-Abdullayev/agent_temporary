609e3afb7b1fd672e9280eb210fc2828bfbf2d7b
abdumalik_s_thinkland <abdumaliksapoqulov@gmail.com>
2022-11-28 11:26:02 +0500

    ..

--- lib/ui/pages/order_page/order_page_widget/market_image_widget.dart
+++ lib/ui/pages/order_page/order_page_widget/market_image_widget.dart
@@ -9,17 +9,18 @@ class MarketImage extends StatelessWidget {
   Widget build(BuildContext context) {
     return Center(
       child: Container(
-          height: 90,
-          width: 90,
-          decoration: BoxDecoration(
-            borderRadius: BorderRadius.circular(8),
-            color: ColorName.primaryDark,
-          ),
-          child: SizedBox(
-            height: 37,
-            width: 43,
-            child: Image.asset(image),
-          )),
+        height: 90,
+        width: 90,
+        decoration: BoxDecoration(
+          borderRadius: BorderRadius.circular(8),
+          color: ColorName.primaryDark,
+        ),
+        child: SizedBox(
+          height: 37,
+          width: 43,
+          child: Image.asset(image),
+        ),
+      ),
     );
   }
 }
