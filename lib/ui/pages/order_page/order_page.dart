4487461db4dd5b17803a0020d5c7997132d0b324
TORNADO <ozodbek@thinkland.uz>
2022-12-10 16:40:17 +0500

    Merge branch '27-obshiy-zadachi-page' of https://gitlab.com/tlps-project/agent into Sprint_6

--- lib/ui/pages/order_page/order_page.dart
+++ lib/ui/pages/order_page/order_page.dart
@@ -6,11 +6,8 @@ import 'package:agent/ui/pages/order_page/order_page_widget/order_appbar_icon_wi
 import 'package:agent/ui/pages/order_page/pages/photo_report_page.dart';
 import 'package:agent/ui/pages/order_page/pages/tabbar_order_page.dart';
 import 'package:agent/ui/pages/remain_page/pages/remains_tabbar_page.dart';
-<<<<<<< HEAD
 import 'package:agent/ui/pages/remain_page/widgets/floating_show_widget.dart';
-=======
-import 'package:agent/ui/pages/remains_page/widgets/floating_show_widget.dart';
->>>>>>> 06466332b66e84d37cf38c6bf03d43956e8478cc
+
 import 'package:agent/ui/pages/return_from_shelf/widget/about_return_widget.dart';
 import 'package:agent/ui/widgets/app_widgets.dart';
 import 'package:flutter/material.dart';
